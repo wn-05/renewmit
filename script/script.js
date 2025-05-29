935a7cf58c753ec26ed195a9b1583273b415e9ed
wn-05 <wn0000505@gmail.com>
2025-05-29 16:52:35 +0900

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,43 @@
+
+    // 메인 배너
+    var swiper1 = new Swiper(".Swiper1", {
+      scrollbar: {
+        el: ".swiper-scrollbar-1",
+        hide: true,
+      },
+      loop: true,
+      autoplay: {
+        delay: 3000,
+        speed: 5000,
+      }
+    });
+
+    // 프로그램
+    var swiper = new Swiper(".Swiper2", {
+      pagination: {
+        el: ".swiper-pagination-2",
+        clickable: true,
+        // type: "fraction",
+      },
+      slidesPerView: 4,
+      slidesPerGroup: 4,
+      spaceBetween: 30,
+      
+      });
+
+
+    // 상품
+
+    var swiper3 = new Swiper(".Swiper3", {
+      pagination: {
+        el: ".swiper-pagination-3",
+      },
+      loop: true,
+      autoplay: {
+        delay: 3000,
+        speed: 3000,
+      }
+
+
+
+    });
