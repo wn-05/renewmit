32b262190fdf159641e3fbfe1bbaecd2a3d0f789
wn-05 <wn0000505@gmail.com>
2025-05-30 10:15:01 +0900

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -1,43 +1,55 @@
 
-    // 메인 배너
-    var swiper1 = new Swiper(".Swiper1", {
-      scrollbar: {
-        el: ".swiper-scrollbar-1",
-        hide: true,
-      },
-      loop: true,
-      autoplay: {
-        delay: 3000,
-        speed: 5000,
-      }
-    });
 
-    // 프로그램
-    var swiper = new Swiper(".Swiper2", {
-      pagination: {
-        el: ".swiper-pagination-2",
-        clickable: true,
-        // type: "fraction",
-      },
-      slidesPerView: 4,
-      slidesPerGroup: 4,
-      spaceBetween: 30,
-      
-      });
+  // SWIPER JS
+  // SWIPER JS
+// 메인 배너
+var swiper1 = new Swiper(".Swiper1", {
+  scrollbar: {
+    el: ".swiper-scrollbar-1",
+    hide: true,
+  },
+  loop: true,
+  autoplay: {
+    delay: 3000,
+    speed: 5000,
+  }
+});
 
+// 프로그램
+var swiper = new Swiper(".Swiper2", {
+  pagination: {
+    el: ".swiper-pagination-2",
+    clickable: true,
+    // type: "fraction",
+  },
+  slidesPerView: 4,
+  slidesPerGroup: 4,
+  spaceBetween: 30,
 
-    // 상품
-
-    var swiper3 = new Swiper(".Swiper3", {
-      pagination: {
-        el: ".swiper-pagination-3",
-      },
-      loop: true,
-      autoplay: {
-        delay: 3000,
-        speed: 3000,
-      }
+});
 
 
+// 상품
 
-    });
+var swiper3 = new Swiper(".Swiper3", {
+  pagination: {
+    el: ".swiper-pagination-3",
+  },
+  loop: true,
+  autoplay: {
+    delay: 3000,
+    speed: 3000,
+  }
+
+
+
+});
+
+
+// AOS JS
+// AOS JS
+AOS.init({
+  duration: 1000,
+  offset: 300,
+  easing: 'ease-out-back',
+});
